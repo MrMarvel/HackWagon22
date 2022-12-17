ee9e7c9ff4a9215c395a8be2e2cd981242fbcc2c
MrMarvel <seregakkk999@yandex.ru>
2022-12-17 22:01:49 +0300

    Добавил получение файла, а также тест

--- server_api/model_api/controllers.py
+++ server_api/model_api/controllers.py
@@ -1,7 +1,27 @@
+import base64
+from wsgiref.util import FileWrapper
+
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 
 
-def audio_to_xml(request: WSGIRequest) -> HttpResponse:
-    print("hi")
-    return HttpResponse("hi")
+def audio_to_table(request: WSGIRequest) -> HttpResponse:
+    if request.content_type in ["multipart/form-data", "application/x-www-form-urlencoded"]:
+        # get file from request body coded by base64 and convert it to .wav file
+        for file in request.FILES.getlist("uploadedfile"):
+            file_str = base64.b64decode(file.read())
+            # read file "test_ouput.csv" and put it to variable "result_table" encoded by base64
+            with open("tests_input/test_output.csv", "rb") as file:
+                file_str = bytes(file.read())
+                result_table_as_bytes = base64.b64encode(file_str)
+                result_table = SimpleUploadedFile("result.csv", result_table_as_bytes, content_type="text/csv")
+                # return status code 200 and response body .csv file
+                return HttpResponse(result_table, content_type="text/csv")
+    else:
+        return HttpResponse("Bad request", status=400)
+
+
+
+
+    pass
--- server_api/model_api/urls.py
+++ server_api/model_api/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from .controllers import audio_to_xml
+from .controllers import audio_to_table
 
 urlpatterns = [
-    path('audio-to-xml/', audio_to_xml),
+    path('audio-to-table/', audio_to_table),
 ]
